9fd4baea01f0121a5db2764bd29f89bb67af8c3d
Lolization <razmatan91@gmail.com>
2020-09-24 08:10:08 +0300

    Added Bot Functionality kinda

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -10,18 +10,46 @@ const config = {
 
 const editor = CodeMirror.fromTextArea(document.getElementById("code"), config);
 
-
-console.log(docHeight = document.body.clientHeight)
-console.log(editor.doc.height)
-console.log((docHeight * 0.7) >> 0)
-CodeMirror.height = docHeight
+// console.log(docHeight = document.body.clientHeight)
+// console.log(editor.doc.height)
+// console.log((docHeight * 0.7) >> 0)
+// CodeMirror.height = docHeight
 
 /*function selectTheme() {
     editor.setOption("theme", "solarized dark");
 }
 setTimeout(selectTheme, 5000);*/
 
+window.onload = function() {
+	AddBots(["super bot 1", "super crazy bot 2"]);
+}
 
 function Submit() {
-    let code = editor.getValue();
+	let code = editor.getValue();
+	console.log(code)
+}
+
+function AddBots(bots) {
+	bots.forEach(botName => {
+		let tr = document.createElement("tr");
+		
+		let nametag = document.createElement("td");
+		let nametext = document.createTextNode(botName);
+		nametag.appendChild(nametext);
+
+		let playtag = document.createElement("td");
+		let playbutton = document.createElement("button");
+		playbutton.setAttribute("class", "play");
+		playbutton.textContent = "Play Against";
+		playtag.appendChild(playbutton);
+
+		tr.appendChild(nametag);
+		tr.appendChild(playtag);
+
+		console.log(tr);
+
+		let table = document.getElementById("tableBots");
+		console.log(table)
+		table.appendChild(tr);
+	});
 }
--- src/server/js/server.js
+++ src/server/js/server.js
@@ -1,12 +1,67 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const parser = require("jsdom");
 
 app.get('/', (req, res) => {
   
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Example app listening at http://localhost:${port}`);
 })
+
+function AddBots(html, bots) {
+	const dom = new parser.JSDOM(html);
+	const document = dom.window.document;
+
+	bots.forEach(botName => {
+		let tr = document.createElement("tr");
+		
+		let nametag = document.createElement("td");
+		let nametext = document.createTextNode(botName);
+		nametag.appendChild(nametext);
+
+		let playtag = document.createElement("td");
+		let playbutton = document.createElement("button");
+		playbutton.setAttribute("class", "play");
+		playbutton.textContent = "Play Against";
+		playtag.appendChild(playbutton);
+
+		tr.appendChild(nametag);
+		tr.appendChild(playtag);
+
+		let table = document.getElementById("tableBots");
+		table.appendChild(tr);
+	});
+
+	html = document.documentElement.innerHTML;
+	return html;
+}
+
+html = `<!DOCTYPE html>
+<html>
+<head>
+  <title>Webkit</title>
+  <link type="text/css" rel="stylesheet" href="/src/client/css/styles.css">
+</head>
+<body>
+  <textarea id="code" class="CodeMirror"></textarea>
+  <link rel="stylesheet" type="text/css" href="../../../node_modules/codemirror/lib/codemirror.css">
+  <link rel="stylesheet" type="text/css" href="../../../node_modules/codemirror/theme/ambiance.css">
+  <link rel="stylesheet" type="text/css" href="../../../node_modules/codemirror/theme/solarized.css">
+  <script type="text/javascript" src="../../../node_modules/codemirror/lib/codemirror.js"></script>
+  <script type="text/javascript" src="../../../node_modules/codemirror/mode/python/python.js"></script>
+  <script type="text/javascript" src="../../../node_modules/codemirror/addon/edit/closebrackets.js"></script>
+  <script type="text/javascript" src="../../../node_modules/codemirror/addon/edit/matchbrackets.js"></script>
+
+  <script type="text/javascript" src="../js/index.js"></script>
+  <button id="submit" onclick="Submit()">Submit!</button>
+
+  <table class="bots" id="tableBots"></table>
+
+</body>
+</html>`
+
+AddBots(html, ["bad dude man 1", "bad dude another man 2"]);
