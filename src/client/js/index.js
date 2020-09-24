3fde54622c4213eb250145d301788bd27914c5ad
Lolization <razmatan91@gmail.com>
2020-09-24 21:05:56 +0300

    Server shit working kinda

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -20,33 +20,7 @@ const editor = CodeMirror.fromTextArea(document.getElementById("code"), config);
 }
 setTimeout(selectTheme, 5000);*/
 
-window.onload = function() {
-	AddBots(["super bot 1", "super crazy bot 2"]);
-}
-
 function Submit() {
 	let code = editor.getValue();
 	console.log(code)
-}
-
-function AddBots(bots) {
-	bots.forEach(botName => {
-		let tr = document.createElement("tr");
-		
-		let nametag = document.createElement("td");
-		let nametext = document.createTextNode(botName);
-		nametag.appendChild(nametext);
-
-		let playtag = document.createElement("td");
-		let playbutton = document.createElement("button");
-		playbutton.setAttribute("class", "play");
-		playbutton.textContent = "Play Against";
-		playtag.appendChild(playbutton);
-
-		tr.appendChild(nametag);
-		tr.appendChild(playtag);
-
-		let table = document.getElementById("tableBots");
-		table.appendChild(tr);
-	});
 }
