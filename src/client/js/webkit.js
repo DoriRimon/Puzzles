ca1bb5e0c2e34086cb3c5993addaf85504decaed
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2020-10-19 10:57:32 +0300

    Bots menu

--- src/client/js/webkit.js
+++ src/client/js/webkit.js
@@ -1,3 +1,4 @@
+const bots = ['Skipper', 'Kowalski', 'Rico', 'Private'];
 const config = {
     lineNumbers: true,
     mode: 'text/x-python',
@@ -52,54 +53,126 @@ function submit() {
         })
     }).then(result => {
         // do something with the result
-        console.log("Submitted with result:", result);
+        console.log('Submitted with result:', result);
     });
 }
 
 runAgainst = (botName) => {
-    fetch("/run", {
+    fetch('/run', {
         headers: {
             'Accept': 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
         },
-        method:"POST",
+        method:'POST',
         body: JSON.stringify({
-            "code": code,
-            "botName": botName
+            'code': code,
+            'botName': botName
         })
     }).then(result => {
         // do something with the result
-        console.log("Ran against bot with result:", result);
+        console.log('Ran against bot with result:', result);
     });
 }
 
-function botsMenu() {
-    
-}
+// todo - bots should be an argument
+function addBots() {
+    let m = document.getElementById('bots-menu');
+    let button = document.getElementById('bots-button');
+    let style = button.style;
+    // if (m != null) {
+    //     if (m.style['visibility'] == '') {
+    //         console.log(m.style['opacity'])
+    //         m.remove();
+    //         // button.style['transform'] = 'rotate(0deg)';
+    //         // setTimeout(() => {
+    //         //     button.style = style;
+    //         // }, 25);
+    //         return;
+    //     }
+    // }
+    let menu = document.createElement('div');
+    menu.id = 'bots-menu';
+    let nav = document.getElementsByClassName('top-section')[0];
+    let code = document.getElementsByClassName('CodeMirror')[0];
 
-function addBots(bots) {
-	bots.forEach(botName => {
-		let tr = document.createElement("tr");
+    let menuStyle = `
+        width: ${(code.offsetWidth + 1)}px;
+        height: fit-content;
+        float: left;
+        /* overflow: scroll; */
+        background-color: #fff;
+        position: absolute;
+        left: 0;
+        top: ${nav.offsetHeight + 3}px;
+        z-index: 100;
+    `
+    menu.setAttribute('style', menuStyle);
+
+    let table = document.createElement('table');
+    table.id = 'bots-table';
+
+    let tableStyle = `
+        table-layout: fixed;
+        width: 100%;
+        border-collapse: collapse;
+        margin-left: 10px;
+        margin-top: ${button.offsetHeight + 10 + 4}px;
+        box-sizing: content-box;
+    `
+    table.setAttribute('style', tableStyle);
+
+    let r = document.createElement('tr');
+    let h1 = document.createElement('th');
+    let h2 = document.createElement('th');
+
+    let h1Text = document.createTextNode('Bots');
+    let h2Text = document.createTextNode('Play');
+    h1.appendChild(h1Text);
+    h2.appendChild(h2Text);
+
+    r.appendChild(h1);
+    r.appendChild(h2);
+    table.appendChild(r)
+    menu.appendChild(table);
+
+
+    buttonStyle = `
+        display: flex;
+        font-size: 16px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        padding: 8px;
+        align-self: center;
+        border: none;
+        border-radius: 4px;
+        background-color: #EFEFEF;
+        transition: 0.2s linear;
+        outline: none;
+        width: 73px;
+        cursor: pointer;
+    `
+    for (bot of bots) {
+        let tr = document.createElement('tr');
 		
-		let nametag = document.createElement("td");
-		let nametext = document.createTextNode(botName);
+		let nametag = document.createElement('td');
+		let nametext = document.createTextNode(bot);
 		nametag.appendChild(nametext);
 
-		let playtag = document.createElement("td");
-		let playbutton = document.createElement("button");
-		playbutton.setAttribute("class", "play");
-		playbutton.setAttribute("onclick", `runAgainst("${botName}")`);
-		playbutton.textContent = "Play Against";
-		playtag.appendChild(playbutton);
+		let playtag = document.createElement('td');
+		let playbutton = document.createElement('button');
+		playbutton.setAttribute('class', 'play');
+		playbutton.setAttribute('onclick', `runAgainst('${bot}')`);
+        playbutton.textContent = 'Play Against ♛';
+
+        playtag.appendChild(playbutton);
 
 		tr.appendChild(nametag);
 		tr.appendChild(playtag);
 
-		let table = document.getElementById("tableBots");
 		table.appendChild(tr);
-	});
-
-	html = document.documentElement.innerHTML;
-	return html;
+    }
+    
+    let leftPanel = document.getElementById('left-panel');
+    leftPanel.appendChild(menu);
 }
 
